f440709cb0186de2be36cf21f9d7efc543fcda84
espoblete <es.poblete.a@gmail.com>
2022-11-23 22:53:24 -0300

    conectado a firebase

--- src/Components/Contacto/Contacto.js
+++ src/Components/Contacto/Contacto.js
@@ -1,17 +1,64 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import {Instagram, Send} from '@mui/icons-material'
+import { addDoc, collection } from "firebase/firestore";
+import { useState } from 'react';
+import { db } from '../Firebase';
 
-const Contacto = () => {
+
+
+export default function Contacto() {
+
+    const [formValues, setFormValues] = useState({
+        name: "",
+        surname: "",
+        email: "",
+        celular: "",
+        contactReason: ""
+    })
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        console.log(formValues)
+        try {
+            const docRef = await addDoc(collection(db, "Contactos"), formValues);
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+
+    function handleFormChange(event) {
+        const { target } = event
+        const { name, value } = target
+        const newValues = { ...formValues, [name]: value }
+        setFormValues(newValues)
+
+    }
+
+
   return (
     <main className='planilla'>
       <h1>¡Contactanos!</h1>
       <h3>Puedes contactarnos a traves de este formulario
         o en nuestras redes sociales.
       </h3>
-    <input type="text" id="nombre"  placeholder="Nombre" />
-    <input type="text" id="apellido"  placeholder="Apellido" />
-    <input type="text" id="correo"  placeholder="Ejemplo@gmail.com" />
+      <form onSubmit={handleSubmit}>
+                    <p><label htmlFor='name'>Nombre</label>
+                    <input id='name' name='name' type="text" value={formValues.name} onChange={handleFormChange} ></input>
+</p>
+                    <p><label htmlFor='surname'>Apellido</label>
+                    <input id='surname' name='surname' type="text" value={formValues.surname} onChange={handleFormChange}></input>
+</p>
+                    <p><label htmlFor='email'>Correo</label>
+                    <input id='email' name='email' type="email" value={formValues.email} onChange={handleFormChange}></input>
+</p>
+                    <p><label htmlFor='contactReason'>Motivo</label>
+                    <textarea id='contactReason' name='contactReason' value={formValues.contactReason} onChange={handleFormChange}></textarea>
+</p>
+                    <button type='submit'>Enviar</button>
+                    </form>
+          
     <Button variant="contained" endIcon={<Send />}>
     Send
     </Button>
@@ -23,4 +70,3 @@ const Contacto = () => {
   )
 }
 
-export default Contacto
